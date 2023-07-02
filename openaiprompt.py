d6687a370e76a563f95235cd1f7b706b8b8f9512
SIDLAD <95582914+SIDLAD@users.noreply.github.com>
2023-07-02 09:47:55 +0530

    openai prompt added
    function getReply in openaiprompt returns the reply on asking chatgpt

---
+++ openaiprompt.py
@@ -0,0 +1,12 @@
+#pip install openai
+#create key.txt and paste your secret api key in it
+import openai
+
+def getReply(promptText):
+    openai.api_key = open("key.txt","r").read().strip("\n")
+    completion =openai.ChatCompletion.create(
+        model ="gpt-3.5-turbo",
+        messages = {"role":"user","content":promptText}
+    )
+    reply_content = completion.choices[0].message.content
+    return reply_content
